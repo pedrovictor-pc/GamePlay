5243f6344e4a665c5a87531f9a8f2dbd0d38616d
Pedro Victor <140213533+pedrovictor-pc@users.noreply.github.com>
2025-09-03 12:22:55 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,22 +1,12 @@
-import {View,StyleSheet, Text, TextInput} from 'react-native';
+import {View,StyleSheet, Text} from 'react-native';
 
 export default function App (){
-
-    return(
-        <View style={styles.container}>
-            <View style={styles.box1}/>
-            <View style={styles.box2}>
-                <Text style={styles.texto}>Hello World</Text>
-            </View>
-            <View style={styles.box3}/>
-        </View>
-    )
+  return(
+    <View>
+      
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container:{flex:1,backgroundColor:'black', justifyContent: 'center', alignItems:'center'},
-    box1:{width:200,height:200,backgroundColor:'#89deffff'},
-    box2:{width:200,height:200,backgroundColor:'#3eee56ff'},
-    box3:{width:200,height:200,backgroundColor:'#cc2a2aff'},
-    texto:{fontSize:40}
 })
