dc60e2fa0dd5c3f7e48a22ea0e55d609b2cb4536
Pedro Victor <140213533+pedrovictor-pc@users.noreply.github.com>
2025-09-10 12:55:10 -0300

    Add files via upload

--- src/screens/SingIn/index.tsx
+++ src/screens/SingIn/index.tsx
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import {View, Text, TextInput} from 'react-native';
-import { stylesSingIn } from './styles';
+import {View, Text, Image} from 'react-native';
+import { styles } from './styles';
+import IllustrationImg from '../../assets/illustration.png';
 
-export function SingIn (){
+export function SingIn(){
     return(
-        <View style={stylesSingIn.container}>
-            <Text style={stylesSingIn.texto}>Digite sua senha:</Text>
-            <TextInput style={stylesSingIn.input}/>
+        <View style={styles.container}>
+            <Image source={IllustrationImg} style={styles.image} resizeMode='stretch'/>
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize suas {'\n'}
+                    Jogatinas {'\n'}
+                    Facilmente
+                </Text>
+            </View>
         </View>
     )
-}
+};
--- src/screens/SingIn/styles.ts
+++ src/screens/SingIn/styles.ts
@@ -1,23 +1,19 @@
 import { StyleSheet } from "react-native"
 
-export const stylesSingIn = StyleSheet.create({
+export const styles = StyleSheet.create({
     container:{
         flex:1,
-        justifyContent: 'center',
-        alignItems:'center',
     },
-    texto:{
-        fontSize:30,
-        textAlign:'center',
-        margin:10
+    image:{
+        width:'100%',
+        height:360
     },
-    input:{
-        marginTop: 20,
-        height:60,
-        width:200,
-        borderBottomWidth: 2,
-        backgroundColor: '#20b9d4ff',
-        fontSize:30,
-        color:'white'
+    content:{
+        
+    },
+    title:{
+        fontSize:25,
+        alignSelf:'center',
+        textAlign:'center'
     }
 })
